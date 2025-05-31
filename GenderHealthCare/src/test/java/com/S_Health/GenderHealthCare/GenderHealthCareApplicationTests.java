d6112a547e4a06dad51b63d001e827e8fb42453e
DangQuangSE <quangndse180246@fpt.edu.vn>
2025-05-31 17:14:51 +0700

    Create GenderHealthCare project

---
+++ GenderHealthCare/src/test/java/com/S_Health/GenderHealthCare/GenderHealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S_Health.GenderHealthCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderHealthCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
