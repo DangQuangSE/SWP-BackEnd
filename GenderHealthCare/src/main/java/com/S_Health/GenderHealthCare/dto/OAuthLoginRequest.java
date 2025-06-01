532cef96797337f416e3965182c2224bf76a30cc
DangQuangSE <quangndse180246@fpt.edu.vn>
2025-06-01 15:39:25 +0700

    Login by Google

---
+++ GenderHealthCare/src/main/java/com/S_Health/GenderHealthCare/dto/OAuthLoginRequest.java
@@ -0,0 +1,12 @@
+package com.S_Health.GenderHealthCare.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OAuthLoginRequest {
+    private String token;
+}
