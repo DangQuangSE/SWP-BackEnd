a8607fb8ab1d2efe1dfd9bce47b863d4d0fc6382
HCodi24 <ntson68199@gmail.com>
2025-07-16 21:56:34 +0700

    test 1

--- src/main/java/com/S_Health/GenderHealthCare/config/Filter.java
+++ src/main/java/com/S_Health/GenderHealthCare/config/Filter.java
@@ -83,7 +83,8 @@ public class Filter extends OncePerRequestFilter {
             "/api/blog/my-blogs",                // User's own blogs
             "/api/blog/detail/*",                // Blog details for editing (specific ID)
             "/api/chat/sessions",                // Staff get chat sessions (GET method)// Staff get session messages (GET method)
-            "/api/zoom/**"// Staff get chat sessions (GET method// Staff get session messages (GET method)
+            "/api/zoom/**",
+            "/api/notifications"// Staff get chat sessions (GET method// Staff get session messages (GET method)
     );
 
     private final List<String> PROTECTED_PATCH_API = List.of(
