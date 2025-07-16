139ee3ffb33d6e6a7f40679a471acae96fbfdf5c
DangQuangSE <quangndse180246@fpt.edu.vn>
2025-07-16 17:27:49 +0700

    fix

--- src/main/java/com/S_Health/GenderHealthCare/enums/ServiceType.java
+++ src/main/java/com/S_Health/GenderHealthCare/enums/ServiceType.java
@@ -3,7 +3,7 @@ package com.S_Health.GenderHealthCare.enums;
 public enum ServiceType {
     CONSULTING,
     CONSULTING_ON,// Tư vấn online
-    TESTING,      // Xét nghiệm trực tuyến
+    TESTING_OFF,      // Xét nghiệm trực tuyến
     TREATMENT,         // Điều trị
     EXAMINATION,
     COMBO// Khám bệnh
